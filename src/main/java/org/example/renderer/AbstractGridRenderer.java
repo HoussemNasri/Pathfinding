06342bbe40317aa13fcffdd38532d7ca62b45a5f
Houssem Nasri <housi.housi2015@gmail.com>
2020-12-25 02:06:34 +0100

    Improve reusability

--- src/main/java/org/example/renderer/AbstractGridRenderer.java
+++ src/main/java/org/example/renderer/AbstractGridRenderer.java
@@ -1,29 +1,86 @@
 package org.example.renderer;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 import org.example.cell.AbstractCellModel;
+import org.example.cell.Point;
 import org.example.cell.state.AbstractGridState;
 import org.example.cell.view.AbstractCellView;
-
 
 /**
  * @param <C> The cell model
  * @param <S> The grid state
  * @param <V> The cell view
- * @param <ViewType> The view of the grid
  * */
-public abstract class AbstractGridRenderer<C extends AbstractCellModel, S extends AbstractGridState<C>, V extends AbstractCellView<? extends Region, C>, ViewType extends Region> implements GridRenderer<ViewType>{
+public abstract class AbstractGridRenderer<C extends AbstractCellModel, S extends AbstractGridState<C>, V extends AbstractCellView<? extends Region, C>> implements GridRenderer<GridPane> {
+    private static final double BORDER_WIDTH = 1;
+    private static final Paint BORDER_PANT = Color.BLACK;
+
     protected int width;
     protected int height;
     protected double cellSize;
     protected S gridState;
+
+    protected GridPane gridView;
 
     public AbstractGridRenderer(S gridState, double cellSize) {
         this.gridState = gridState;
         this.cellSize = cellSize;
         this.width = gridState.getWidth();
         this.height = gridState.getHeight();
+        this.gridView = new GridPane();
+        styleGridView();
+    }
+
+    private void styleGridView() {
+        // turn layout pixel snapping off on the grid so that grid lines will be an even width.
+        gridView.setSnapToPixel(false);
+        gridView.setBorder(createBorder());
+        // style the grid so that it has a background and gaps around the grid and between the
+        // grid cells so that the background will show through as grid lines.
+        gridView.setBackground(createBackground());
+        gridView.setHgap(BORDER_WIDTH);
+        gridView.setVgap(BORDER_WIDTH);
+    }
+
+    private Border createBorder() {
+        BorderWidths borderWidths = new BorderWidths(BORDER_WIDTH);
+        BorderStroke borderStroke = new BorderStroke(null, null, null, borderWidths);
+        Border border = new Border(borderStroke);
+
+        return border;
+    }
+
+    private Background createBackground() {
+        BackgroundFill grisViewFill = new BackgroundFill(BORDER_PANT, CornerRadii.EMPTY, Insets.EMPTY);
+        Background gridViewBackground = new Background(grisViewFill);
+
+        return gridViewBackground;
+    }
+
+    protected abstract V createCellView(Point coordinate);
+
+    @Override
+    public void render() {
+        gridView.setMinSize(cellSize * width, cellSize * height);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                AbstractCellView<?, C> cellView = createCellView(Point.of(x, y));
+                GridPane.setColumnIndex(cellView.getView(), x);
+                GridPane.setRowIndex(cellView.getView(), y);
+                gridView.add(cellView.getView(), x, y);
+            }
+        }
     }
 
     public S getGridState() {
--- src/main/java/org/example/renderer/AstarGridRenderer.java
+++ src/main/java/org/example/renderer/AstarGridRenderer.java
@@ -17,57 +17,15 @@ import org.example.cell.Point;
 import org.example.cell.state.AstarGridState;
 import org.example.cell.view.AstarCellView;
 
-public class AstarGridRenderer extends AbstractGridRenderer<AstarCell, AstarGridState, AstarCellView, GridPane> {
-    private static final double BORDER_WIDTH = 1;
-    private static final Paint BORDER_PANT = Color.BLACK;
-    private final GridPane gridView = new GridPane();
+public class AstarGridRenderer extends AbstractGridRenderer<AstarCell, AstarGridState, AstarCellView> {
 
     public AstarGridRenderer(AstarGridState gridState, double cellSize) {
         super(gridState, cellSize);
-        styleGridView();
         render();
     }
 
-    private void styleGridView() {
-        // turn layout pixel snapping off on the grid so that grid lines will be an even width.
-        gridView.setSnapToPixel(false);
-        gridView.setBorder(createBorder());
-        // style the grid so that it has a background and gaps around the grid and between the
-        // grid cells so that the background will show through as grid lines.
-        gridView.setBackground(createBackground());
-        gridView.setHgap(BORDER_WIDTH);
-        gridView.setVgap(BORDER_WIDTH);
-    }
-
-    private Border createBorder() {
-        BorderWidths borderWidths = new BorderWidths(BORDER_WIDTH);
-        BorderStroke borderStroke = new BorderStroke(null, null, null, borderWidths);
-        Border border = new Border(borderStroke);
-
-        return border;
-    }
-
-    private Background createBackground() {
-        BackgroundFill grisViewFill = new BackgroundFill(BORDER_PANT, CornerRadii.EMPTY, Insets.EMPTY);
-        Background gridViewBackground = new Background(grisViewFill);
-
-        return gridViewBackground;
-    }
-
     @Override
-    public void render() {
-        gridView.setMinSize(cellSize * width, cellSize * height);
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                AstarCellView cellView = createCellView(Point.of(x, y));
-                GridPane.setColumnIndex(cellView.getView(), x);
-                GridPane.setRowIndex(cellView.getView(), y);
-                gridView.add(cellView.getView(), x, y);
-            }
-        }
-    }
-
-    private AstarCellView createCellView(Point cellCoordinate) {
+    protected AstarCellView createCellView(Point cellCoordinate) {
         return new AstarCellView(cellSize, gridState.getCell(cellCoordinate));
     }
 
